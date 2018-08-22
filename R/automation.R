ede3cfe33c1544e1ae03060ddc2cafac8b7f0225
reneechou123 <reneechou123@gmail.com>
2018-08-22 16:10:25 -0400

    Update automation.R

--- R/automation.R
+++ R/automation.R
@@ -2,7 +2,7 @@
 automation <- function(rootDir, geneCol, x.threshold=0.05, y.threshold=0.05, adjPvalue=TRUE, qqPlot=TRUE, fishPlot=TRUE, scatterPlot=TRUE){
   path <- paste(system.file(package="DGEclustering"), "automation.py", sep="/")
   system(paste(path,
-               '-d', roodDir,
+               '-d', rootDir,
                '-g', geneCol,
                '-x', x.threshold,
                '-y', y.threshold,
