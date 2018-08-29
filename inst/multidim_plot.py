d58552ffd9b3a05733a20d0198ef687fdb6b975b
reneechou123 <reneechou123@gmail.com>
2018-08-29 16:29:17 -0400

    Update multidim_plot.py

--- inst/multidim_plot.py
+++ inst/multidim_plot.py
@@ -14,6 +14,7 @@ def main():
     parser.add_argument('-x', '--x_threshold', default=0.05, type=float, help='(adjusted) pvalue for scatter plot x axis')
     parser.add_argument('-y', '--y_threshold', default=0.05, type=float, help='(adjusted) pvalue for scatter plot y axis')
     parser.add_argument('-a', '--adj_pvalue', default=1, type=int, help='whether to use adjusted pvalue or pvalue. 1 as True, 0 as False'
+    args = parser.parse_args()
 
     warnings.filterwarnings('ignore') # ignore runtime warnings
     temp = plotting.scatter_plot(file_paths=args.file_paths, x_file_number=args.x_file_number, y_file_number=args.y_file_number, plot_out_dir=args.plot_dir, dat_out_dir=args.dat_dir, x_threshold=args.x_threshold, y_threshold=args.y_threshold, adj_pvalue=args.adj_pvalue, return_sig_plot=True, out_file_name=args.output)
