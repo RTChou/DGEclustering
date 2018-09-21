081867da32b3bf5a8b54fd7218d4ef4101597a3f
reneechou123 <reneechou123@gmail.com>
2018-09-21 13:56:18 -0400

    Update annotate.genes.R

--- R/annotate.genes.R
+++ R/annotate.genes.R
@@ -1,28 +1,43 @@
+#' @title annotate genes with GO terms
+#'
+#' @description Generate a GO term binary matrix from a paired DGE dataset
+#' @param OrgDb object returned by AnnotationHub.
+#' @param keyType type of genes. e.g. 'ENSEMBL', 'ENTREZID', 'SYMBOL'.
+#' @param genes a vector of genes.
+#' @param GOEnrichment if TRUE, annotate genes with GO enrichment. Default FALSE.
+#' @param BgGenes background genes if use GO enrichment.
+#' @param ont One of 'ALL', 'BP', 'BF', or 'CC'.
 #' @export
 #' @import clusterProfiler
-annotate.genes <- function(OrgDb, keyType, pairedDataset, geneCol, GOEnrichment=FALSE, BgGenes=NULL, ont='ALL'){
-  dataset <- pairedDataset
-  universe <- BgGenes
-  ds.keys <- sapply(dataset[geneCol], as.character)
-  unv.keys <- sapply(universe, as.character)
+#' @return ann a dataframe of the GO term binary matrix
+#' @examples  \dontrun{}
+annotate.genes <- function(OrgDb, keyType, genes, GOEnrichment=FALSE, BgGenes=NULL, ont='ALL'){
+  ds.keys <- sapply(genes, as.character)
+  unv.keys <- sapply(BgGenes, as.character)
 
   if (GOEnrichment == FALSE){
     # assign GO terms directly
-    GO.res <- select(OrgDb, ds.keys, c(keyType, 'GO'), keyType)
+    GO.res <- select(OrgDb, ds.keys, c(keyType, 'GO', 'ONTOLOGY'), keyType)
     GO.res <- GO.res[complete.cases(GO.res['GO']),]
+    if (ont != 'ALL'){
+      GO.res[GO.res['ONTOLOGY'] == ont,]
+    }
   }
   else {
     # GO enrichment
-    GO.res <- enrichGO(gene=dataset$EntrezID, OrgDb=OrgDb, keyType=keyType, ont=ont, universe=universe)
+    GO.res <- enrichGO(gene=dataset, OrgDb=OrgDb, keyType=keyType, ont=ont, universe=unv.keys)
   }
 
   # construct binary GO term matrix
+  ann <- data.frame(genes=ds.keys)
+  rownames(ann) <- ds.keys
   for (row in 1:nrow(GO.res)){
     GO.term <- GO.res[row, 'GO']
-    if (!GO.term %in% colnames(dataset))
-      dataset[GO.term] <- 0
-    dataset[dataset[geneCol]==GO.res[row, 'SYMBOL'], GO.term] <- 1
+    if (!GO.term %in% colnames(ann))
+      ann[GO.term] <- 0
+    ann[rownames(dataset)==GO.res[row, 'SYMBOL'], GO.term] <- 1
   }
-  return(dataset)
+  ann <- ann[,-1]
+  return(ann)
 }
 
