90b2ccdf6695b0470957ad849d1a48e2285c576a
Renee Ti Chou <reneechou123@gmail.com>
2018-10-03 17:14:27 -0400

    Added data.R

---
+++ R/data.R
@@ -0,0 +1,55 @@
+#' Differential expression dataset 1
+#'
+#' RNA-seq differential gene expression dataset analyzed using DESeq2.
+#' The raw data is retrieved from \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE93737}.
+#' The experimental condition is arbitrarily assigned as treatment1 vs control.
+#' 
+#' @format A data frame with 17402 rows and 7 variables
+#' \describe{
+#'   \item{gene}
+#'   \item{baseMean}
+#'   \item{log2FoldChange}
+#'   \item{lfcSE}
+#'   \item{stat}
+#'   \item{pvalue}
+#'   \item{padj}
+#' }
+"treatment1.vs.control"
+
+
+#' Differential expression dataset 2
+#'
+#' RNA-seq differential gene expression dataset analyzed using DESeq2.
+#' The raw data is retrieved from \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE93737}.
+#' The experimental condition is arbitrarily assigned as treatment2 vs control.
+#' 
+#' @format A data frame with 17402 rows and 7 variables
+#' \describe{
+#'   \item{gene}
+#'   \item{baseMean}
+#'   \item{log2FoldChange}
+#'   \item{lfcSE}
+#'   \item{stat}
+#'   \item{pvalue}
+#'   \item{padj}
+#' }
+"treatment2.vs.control"
+
+
+#' Differential expression dataset 3
+#'
+#' RNA-seq differential gene expression dataset analyzed using DESeq2.
+#' The raw data is retrieved from \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE93737}.
+#' The experimental condition is arbitrarily assigned as treatment3 vs control.
+#' 
+#' @format A data frame with 17402 rows and 7 variables
+#' \describe{
+#'   \item{gene}
+#'   \item{baseMean}
+#'   \item{log2FoldChange}
+#'   \item{lfcSE}
+#'   \item{stat}
+#'   \item{pvalue}
+#'   \item{padj}
+#' }
+"treatment3.vs.control"
