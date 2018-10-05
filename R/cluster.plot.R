2c57f330c458f3fa8612c68489790520e72fa3c7
Renee Ti Chou <reneechou123@gmail.com>
2018-10-05 14:58:54 -0400

    Update cluster.plot.R

--- R/cluster.plot.R
+++ R/cluster.plot.R
@@ -3,17 +3,37 @@
 #'   and also the path to the clustering result file, 
 #'   this will generate a set clustered scatter/MCA plots.
 #' @param outDir directory for storing the output plots
-#' @param filePaths a list of dataset file paths or the file path to the MCA result
+#' @param datasets a list of datasets
 #' @param MCA If FALSE (which is Default), generate clustered scatter plots
-#' @param x.fileNumber the index of the file in the list to be plotted as x axis
-#' @param y.fileNumber the index of the file in the list to be plotted as y axis
+#' @param x.dsNumber the index of the dataset in the list to be plotted as x axis
+#' @param y.dsNumber the index of the dataset in the list to be plotted as y axis
 #' @param geneCol name of the column where the genes are stored in the datasets
 #' @param clusterFilePath the file path to the clustering result
 #' @param adjPvalue if TRUE, use adjusted p-value for the threshold; if FALSE, use p-value
 #' @param color type of matplotlib colormap
 #' @export
 #' @examples  \dontrun{}
-cluster.plot <- function(outDir='./', filePaths, MCA=FALSE, x.fileNumber=1, y.fileNumber=2, geneCol, clusterFilePath, adjPvalue=TRUE, color='brg'){
+cluster.plot <- function(datasets, res.groups, res.MCA, MCA=FALSE, x.dsNumber=1, y.dsNumber=2, geneCol, adjPvalue=TRUE, color='brg'){
+  temp.folder <- '/tmp/dgeclustering'
+  system(paste('mkdir -p', temp.folder))
+  if (MCA == FALSE) {
+    if (is.null(datasets) | is.null(res.groups)){i
+      stop('arguments \"datasets\" and \"res.groups\" should not be NULL.')
+    }
+    filepaths <- file.path(temp.folder, paste0(names(datasets), '.tsv'))
+    cluster.filepath <- file.path(temp.folder, 'clusters.tsv')
+    for (i in 1:length(datasets)){
+      write.table(datasets[[i]], file=filepaths[i], sep='\t', row.names=FALSE)
+    }
+  }  
+  else {
+    if (is.null(res.groups) | is.null(res.MCA)) {
+      stop('\"res.groups\" and \"res.MCA\" should not be NULL.')
+    }
+    filepaths <- file.path(temp.folder, 'MCA.tsv'))
+    write.table(res$MCA, 'MCA.tsv', sep='\t', row.names=TRUE, col.names=NA)
+  }
+  write.table(stack(res.groups), file=cluster.filepath, sep='\t', row.names=FALSE)
   python.boolean.convert <- function(bool) {
     if (bool == TRUE)
       return('1')
@@ -22,14 +42,13 @@ cluster.plot <- function(outDir='./', filePaths, MCA=FALSE, x.fileNumber=1, y.fi
   }
   path <- paste(system.file(package='DGEclustering'), 'cluster_plot.py', sep='/')
   system(paste(path,
-               '-d', outDir,
+               '-d', temp.folder,
                '-f', paste(filePaths, collapse=' '),
                '-m', python.boolean.convert(MCA),
-               '-n1', x.fileNumber - 1,
-	       '-n2', y.fileNumber - 1,
+               '-n1', x.dsNumber - 1,
+	       '-n2', y.dsNumber - 1,
 	       '-g', geneCol,
-	       '-r', clusterFilePath,
+	       '-r', cluster.filepath,
                '-a', python.boolean.convert(adjPvalue),
                '-c', color))
 }
-
